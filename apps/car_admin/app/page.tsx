6f0d61ec8f86d4c9486975d9df849b288a7d6042
moses mwangi <moses.mwangi.me@gmail.com>
2024-12-21 20:44:44 +0300

    commit

--- apps/car_admin/app/page.tsx
+++ apps/car_admin/app/page.tsx
@@ -6,6 +6,9 @@ export default function Home() {
       <p className=" text-xl font-bold text-blue-900">
         This is a car admin dash board yes
       </p>
+      <button className=" px-2 py-1 flex items-center justify-center text-xl text-blue-500 bg-slate-300">
+        Click Me
+      </button>
     </div>
   );
 }
