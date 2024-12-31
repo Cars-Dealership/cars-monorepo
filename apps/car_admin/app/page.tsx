1d1e8efb5721a208264bd0069f413c7f14a9bc70
moses mwangi <moses.mwangi.me@gmail.com>
2024-12-31 19:25:19 +0300

    commit

--- apps/car_admin/app/page.tsx
+++ apps/car_admin/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <p className=" text-xl font-bold text-blue-900">
         This is a car admin dash board yes
       </p>
-      <button className=" px-2 py-1 flex items-center justify-center text-xl text-blue-500 bg-slate-300">
+      <button className=" hover:bg-red-700 hover:text-blue-700 px-2 py-1 flex items-center justify-center text-xl text-blue-500 bg-slate-300">
         Click Me
       </button>
     </div>
